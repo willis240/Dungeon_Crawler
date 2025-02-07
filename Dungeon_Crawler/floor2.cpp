a77ea675d95ef75c38e30a56e5de83c8f3c5a8c7
willis240 <willingness240@gmail.com>
2025-02-06 21:14:30 -0900

    added doors to Hall of Resolve F2

--- Dungeon_Crawler/floor2.cpp
+++ Dungeon_Crawler/floor2.cpp
@@ -24,6 +24,15 @@ void floor2(vector<Player>& players, int& roomNum, int& floorNum, Inventory& inv
 		"The Staircase Door, now unlocked, acts as a simple passage between the front entryway and the second floor of \n"
 		"Aria's house.");
 	auto staircaseDoorPtr = make_shared<Door>(staircaseDoor);
+	Door yellowDoor(make_pair(0, 1), "Yellow Door", { "yellow door" }, false,
+		"A standard wooden door, but with a lemon yellow hue. The color makes it fairly distinct.");
+	auto yellowDoorPtr = make_shared<Door>(yellowDoor);
+	Door dentedDoor(make_pair(0, 2), "Dented Door", { "dented door" }, true, true, 14,
+		"A standard wooden door, but- \n \n"
+		"\"WHO put this big freaking dent in my door?!\" \n \n"
+		"...Yeah, that's not coming out. \n \n"
+		"\"These jerks come into my house and take over, and they can't even treat it with RESPECT!\" Aria clenches \n"
+		"her fist. \"Ohhh, I'm going to kill whoever did this.\"");
 	Room hallOfResolve(0, "Hall of Resolve", {}, { staircaseDoorPtr }, {}, {});
 
 	while (true)
@@ -33,7 +42,10 @@ void floor2(vector<Player>& players, int& roomNum, int& floorNum, Inventory& inv
 		if (roomNum == 0)
 		{
 			system("CLS");
-			cout << "You've made it to Floor 2!!!";
+			cout << "The hallway on the second floor has a few doors. There is the Staircase Door which \n";
+			cout << "leads back to the first floor, a Yellow Door, and a Dented Door. The hall itself is perpendicular \n";
+			cout << "to the top of the stairs, with the Yellow Door on the left side of the hall, and the Dented \n";
+			cout << "Door on the right.";
 			dblEndl();
 			checkInput(roomNum, floorNum, players, inventory, hallOfResolve);
 		}
